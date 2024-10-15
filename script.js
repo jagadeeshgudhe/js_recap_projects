d9014346f078139a0172da48f590cc18fb66615d
Gudhe Jagadeesh <jagadeesh@example.com>
2024-10-15 22:34:34 +0530

    counter  project 1

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const increaseBtn = document. getElementById("increaseBtn");
 const countlabel = document.getElementById("countLabel");
 let count = 0;
 increaseBtn.onclick = function(){
-count++;
-countLabel.textContent = count;
+    count++;
+    countLabel.textContent = count;
 }
 decreaseBtn.onclick = function(){
-count--;
-countLabel. textContent = count;
+    count--;
+    countLabel.textContent = count;
 }
 resetBtn.onclick = function(){
-count = 0;
-countLabel, textContent = count;
+    count = 0;
+    countLabel.textContent = count;
 }
